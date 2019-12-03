0717efa5cfc3c97c06b539a1f455d1c2235ce6be
CY <285653184@qq.com>
2019-12-03 09:48:25 +0800

    py qqzone

--- learn/后端/python/爬虫/qzone/qq.py
+++ learn/后端/python/爬虫/qzone/qq.py
@@ -3,9 +3,20 @@ import cookie
 import requests
 from utils import *
 from getpass import getpass
+import time
+
+results_path = './results'
 
 
-# 获取谁关心我
+# 初始化目录
+def init():
+    if not os.path.exists(default_path):
+        os.mkdir(default_path)
+    if not os.path.exists('./results'):
+        os.mkdir('./results')
+
+
+# 谁在意我
 def get_cared_me(qq, gtk, headers):
     url = 'https://h5.qzone.qq.com/proxy/domain/r.qzone.qq.com/cgi-bin/tfriend/friend_ship_manager.cgi?uin={}&do=1&rd=0.11376390567557748&fupdate=1&clean=0&g_tk={}'
     url = url.format(qq, gtk)
@@ -19,23 +30,108 @@ def get_cared_me(qq, gtk, headers):
         exit(-1)
 
     content = res.text
-    print(content)
     save_html(content, './results/{}'.format(qq), 'cared_me.txt')
     return content
 
 
-# 初始化目录
-def init():
-    if not os.path.exists(default_path):
-        os.mkdir(default_path)
-    if not os.path.exists('./results'):
-        os.mkdir('./results')
+# 我在意谁
+def get_me_cared(qq, gtk, headers):
+    url = 'https://h5.qzone.qq.com/proxy/domain/r.qzone.qq.com/cgi-bin/tfriend/friend_ship_manager.cgi?uin={}&do=1&rd=0.11376390567557748&fupdate=1&clean=0&g_tk={}'
+    url = url.format(qq, gtk)
+    res = requests.get(url, headers=headers)
+
+    if res.status_code != 200:
+        print('[Error]: Failed to get me cared')
+        exit(-1)
+
+    content = res.text
+    save_html(content, './results/{}'.format(qq), 'me_cared.txt')
+    return content
+
+
+# 下载 关心 id 列表
+def get_cared_info(qq, gtk, headers, savefile=results_path):
+    savefile = os.path.join(savefile, qq, 'cared_list.txt')
+    cared_me = get_cared_me(qq, gtk, headers)
+    me_cared = get_me_cared(qq, gtk, headers)
+    f_cared_me = re.findall(r'"uin":(.*?),', str(cared_me))
+    f_me_cared = re.findall(r'"uin":(.*?),', str(me_cared))
+
+    f = open(savefile, 'w', encoding='utf-8')
+    f.write('cared_me: \n')
+    for f_id in f_cared_me:
+        f_id = f_id.strip()
+        if f_id:
+            f.write(str(f_id) + ' ')
+    f.write('\n\n\n')
+
+    f.write('me_cared: \n')
+    for f_id in f_me_cared:
+        f_id = f_id.strip()
+        if f_id:
+            f.write(str(f_id) + ' ')
+    print('[INFO]: Get care and cared successfully')
+
+
+# 下载单个好友数据
+def get_friends_info(qq, t_qq, gtk, headers, faillog=results_path):
+    url = 'https://h5.qzone.qq.com/proxy/domain/base.qzone.qq.com/cgi-bin/user/cgi_userinfo_get_all?uin={}&vuin={}&fupdate=1&g_tk={}'
+    url = url.format(t_qq, qq, gtk)
+    res = requests.get(url, headers=headers)
+    results_path = os.path.join(faillog, qq)
+
+    if (res.status_code != 200):
+        print('[Error]: Failed to get friends info')
+        # 写入错误日志
+        f = open(os.path.join(results_path, '{}_fail.txt'.format(qq), 'a'))
+        f.write(str(t_qq) + '\n')
+        f.close()
+        return None
+
+    content = res.text
+    save_html(content, results_path, '{}_info.txt'.format(t_qq))
+    return content
+
+
+# 解析获取所有好友信息
+def get_all_friends_info(qq, friends):
+    friends_info_dict = {}
+    for friend in friends:
+        info_dict = parse_friends_info(qq, friend)
+        if info_dict is not None:
+            friends_info_dict[friend] = info_dict
+    return friends_info_dict
 
 
 if __name__ == '__main__':
     init()
     username = '291825458'
-    # password = 'password'
-    password = getpass('请输入密码:')
-    qq, gtk, headers = cookie.get(username, password)
-    get_cared_me(qq, gtk, headers)
+    password = 'password'
+    # password = getpass('请输入密码:')
+    # qq, gtk, headers = cookie.get(username, password)
+    # 下载好友qq列表
+    # get_cared_info(qq, gtk, headers)
+    # 从文件里读qq列表
+    friends = read_cared()
+
+    # 下载好友信息文件
+    # for friend_id in friends:
+    #     get_friends_info(qq, friend_id, gtk, headers)
+    #     time.sleep(0.2)
+    friends_info_dict = get_all_friends_info(username, friends)
+    # print(friends_info_dict)
+
+    # 处理数据
+    # 地区
+    # data = count_area(friends_info_dict, 'province')
+    # draw_map(data)
+
+    # 性别
+    # data = count_sex(friends_info_dict)
+    # draw_pie([['boy', 'girl', 'other'], list(data)])
+    # print(data)
+
+    # 年龄
+    data = count_age(friends_info_dict)
+    draw_bar(data)
+    print(data)
--- learn/后端/python/爬虫/qzone/utils.py
+++ learn/后端/python/爬虫/qzone/utils.py
@@ -1,5 +1,10 @@
+# -*- coding: UTF-8 -*-
 import os
 import re
+import time
+import datetime
+from pyecharts.charts import Map, Pie, Bar
+from pyecharts import options
 
 default_path = './data'
 
@@ -75,7 +80,233 @@ def get_skey(cookie):
     return item[0] if len(item) > 0 else None
 
 
+# 读取好友列表
+def read_cared(datafile='./results/291825458/cared_list.txt'):
+    friends = []
+    with open(datafile) as f:
+        for line in f:
+            line = line.strip()
+            if line:
+                try:
+                    int(line[0])
+                except:
+                    continue
+                fds = line.split(' ')
+                for fd in fds:
+                    if fd not in friends:
+                        friends.append(fd)
+    return friends
+
+
+# 解析好友文件的数据
+def parse_friends_info(qq, t_qq, datafile='./results'):
+    info_txt = os.path.join(datafile, str(qq), str(t_qq) + '_info.txt')
+    if not os.path.exists(info_txt):
+        return None
+    # 处理信息
+    infoDict = {}
+    with open(info_txt, encoding='utf-8') as f:
+        for line in f:
+            line = line.strip()
+            # 昵称
+            if line.startswith('"nickname":'):
+                nickname = re.findall(r'"nickname":"(.*?)",', line)[0]
+                infoDict['nickname'] = nickname
+                continue
+            # 空间名
+            elif line.startswith('"spacename":'):
+                spacename = re.findall(r'"spacename":"(.*?)",', line)[0]
+                infoDict['spacename'] = spacename
+                continue
+            # 空间简介
+            elif line.startswith('"desc":'):
+                desc = re.findall(r'"desc":"(.*?)",', line)[0]
+                infoDict['desc'] = desc
+                continue
+            # 空间签名
+            elif line.startswith('"signature":'):
+                signature = re.findall(r'"signature":"(.*?)",', line)[0]
+                infoDict['signature'] = signature
+                continue
+            # 性别
+            elif line.startswith('"sex":'):
+                sex = re.findall(r'"sex":(.*?),', line)[0]
+                infoDict['sex'] = sex
+                continue
+            # 出生年
+            elif line.startswith('"birthyear":'):
+                birthyear = re.findall(r'"birthyear":(.*?),', line)[0]
+                infoDict['birthyear'] = birthyear
+                continue
+            # 出生月日
+            elif line.startswith('"birthday":'):
+                birthday = re.findall(r'"birthday":"(.*?)",', line)[0]
+                infoDict['birthday'] = birthday
+                continue
+            # 血型
+            elif line.startswith('"bloodtype":'):
+                bloodtype = re.findall(r'"bloodtype":(.*?),', line)[0]
+                infoDict['bloodtype'] = bloodtype
+                continue
+            # 星座
+            elif line.startswith('"constellation":'):
+                constellation = re.findall(r'"constellation":(.*?),', line)[0]
+                infoDict['constellation'] = constellation
+                continue
+            # 国家
+            elif line.startswith('"country":'):
+                country = re.findall(r'"country":"(.*?)",', line)[0]
+                infoDict['country'] = country
+                continue
+            # 省
+            elif line.startswith('"province":'):
+                province = re.findall(r'"province":"(.*?)",', line)[0]
+                infoDict['province'] = province
+                continue
+            # 城市
+            elif line.startswith('"city":'):
+                city = re.findall(r'"city":"(.*?)",', line)[0]
+                infoDict['city'] = city
+                continue
+            # 家乡国
+            elif line.startswith('"hco":'):
+                hco = re.findall(r'"hco":"(.*?)",', line)[0]
+                infoDict['hco'] = hco
+                continue
+            # 家乡省
+            elif line.startswith('"hp":'):
+                hp = re.findall(r'"hp":"(.*?)",', line)[0]
+                infoDict['hp'] = hp
+                continue
+            # 家乡城
+            elif line.startswith('"hc":'):
+                hc = re.findall(r'"hc":"(.*?)",', line)[0]
+                infoDict['hc'] = hc
+                continue
+            # 婚否
+            elif line.startswith('"marriage":'):
+                marriage = re.findall(r'"marriage":(.*?),', line)[0]
+                infoDict['marriage'] = marriage
+                continue
+            # 职业
+            elif line.startswith('"career":'):
+                career = re.findall(r'"career":"(.*?)",', line)[0]
+                infoDict['career'] = career
+                continue
+            # 公司
+            elif line.startswith('"company":'):
+                company = re.findall(r'"company":"(.*?)",', line)[0]
+                infoDict['company'] = company
+                continue
+            # 最后修改时间
+            elif line.startswith('"ptimestamp":'):
+                ptimestamp = re.findall(r'"ptimestamp":(.*?)}', line)[0]
+                if ptimestamp != '':
+                    temp = time.localtime(float(ptimestamp))
+                    createtime = time.strftime('%Y-%m-%d %H:%M:%S', temp)
+                else:
+                    createtime = ''
+                infoDict['createtime'] = createtime
+                continue
+    return infoDict
+
+
+# 地区统计
+def count_area(friends_dict, area_type):
+    area_dict = {}
+    for key in friends_dict:
+        try:
+            area = friends_dict[key][area_type]
+        except:
+            area = ''
+        if area == '':
+            area = 'unknown'
+        area_dict[area] = area_dict.get(area, 0) + 1
+    return [list(area_dict.keys()), list(area_dict.values())]
+
+
+# 性别统计
+def count_sex(friends_dict):
+    boy = 0
+    girl = 0
+    other = 0
+    for key in friends_dict:
+        try:
+            if friends_dict[key]['sex'] == '2':
+                girl += 1
+            elif friends_dict[key]['sex'] == '1':
+                boy += 1
+            else:
+                other += 1
+        except:
+            other += 1
+    return boy, girl, other
+
+
+# 年龄统计
+def count_age(friends_dict):
+    age_dict = {}
+    for key in friends_dict:
+        try:
+            birth_year = friends_dict[key]['birthyear']
+        except:
+            birth_year = ''
+        if birth_year == '' or birth_year == '0':
+            age_dict['other'] = age_dict.get('other', 0) + 1
+        else:
+            age_dict[birth_year] = age_dict.get(birth_year, 0) + 1
+
+    items = sorted(age_dict.items(), key=lambda x: x[0], reverse=True)
+    counts = []
+    ages = []
+    for item in items:
+        birth_year = item[0]
+        count = item[1]
+        if birth_year != 'other':
+            age = datetime.datetime.now().year - int(birth_year)
+            ages.append(age)
+            counts.append(count)
+        else:
+            ages.append('unknown')
+            counts.append(count)
+    return [ages, counts]
+
+
+# 画地图
+def draw_map(data, map_name='好友地图分布'):
+    option = options.InitOpts(width=1200, height=1200)
+    map_ = (
+        Map(option)
+            .add('重庆', [list(z) for z in zip(data[0], data[1])], 'china')
+            .set_global_opts(title_opts=options.TitleOpts(title=map_name))
+    )
+
+    map_.render('{}.html'.format(map_name))
+
+
+# 画性别饼图
+def draw_pie(data, pie_name='性别统计'):
+    # print([list(z) for z in zip(data[0], data[1])])
+    pie = (
+        Pie()
+            .add('', [list(z) for z in zip(data[0], data[1])])
+            .render('{}.html'.format(pie_name))
+    )
+
+
+# 画年龄柱状
+def draw_bar(data, mark_point=['min', 'max'], bar_name='年龄图'):
+    opt = options.MarkPointOpts(mark_point)
+    bar = (
+        Bar()
+            .add_xaxis(data[0])
+            .add_yaxis('A', data[1])
+            .set_global_opts(title_opts=options.TitleOpts(title='标题', subtitle='我是副标题'))
+            .render('{}.html'.format(bar_name))
+    )
+    return bar
+
+
 if __name__ == '__main__':
-    # save_cookie(cookie)
-    c = read_cookie()
-    print(c)
+    i = parse_friends_info(291825458, 272721580)
+    print(i)
